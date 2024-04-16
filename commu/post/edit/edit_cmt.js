632907fd38c26598df73b178a788bb088a3f212c
kde3 <dekim234@gmail.com>
2024-04-16 19:10:30 +0900

    댓글 디자인 변경 완료

--- commu/post/edit/edit_cmt.js
+++ commu/post/edit/edit_cmt.js
@@ -5,12 +5,12 @@ let comments = [];
 function addComment(event) {
     event.preventDefault();
     const commentInput = document.getElementById('commentInput');
-    var commentText = commentInput.value;
-    var date = new Date().toLocaleString();
+    let commentText = commentInput.value;
+    let date = new Date().toLocaleString();
     
-    if(commentText){
+    if(commentText) {
 
-        var commentDiv = document.createElement("div");
+        let commentDiv = document.createElement("div");
         commentDiv.className = "card";
         commentDiv.innerHTML = `
         <div class="card-body">
@@ -68,10 +68,11 @@ function addComment(event) {
 
         console.log(commentDiv);
         
-        var commentsDiv = event.target.parentNode.querySelector(".comments");
+        let commentsDiv = event.target.parentNode.querySelector(".comments");
         commentsDiv.insertBefore(commentDiv, commentsDiv.firstChild);
         commentInput.value = "";
-    }else{
+    }
+    else {
         alert("입력값이 없습니다"); 
     }
 }
