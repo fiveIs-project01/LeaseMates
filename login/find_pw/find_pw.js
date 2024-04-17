afb0702dd192649587e33092b57da7cb761b9b00
minkyong00 <minkyong131@gmail.com>
2024-04-17 13:57:30 +0900

    header error 수정, 로그인 경로 수정

---
+++ login/find_pw/find_pw.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 비밀번호 찾기 버튼 불러오기
+    const findPwButton = document.querySelector('.find-pw-button');
+
+    // 비밀번호 찾기 버튼 클릭 시 알림창 출력 및 로그인 페이지로 이동
+    findPwButton.addEventListener('click', function(event) {
+        // 기본 동작 취소 (페이지 새로고침 방지)
+        event.preventDefault();
+
+        // 아이디와 이름이 입력되었는지 확인
+        const idInput = document.getElementById('id').value;
+        const nameInput = document.getElementById('name').value;
+
+        if (idInput && nameInput) {
+            // 입력이 있을 경우 알림창 출력
+            alert("비밀번호 찾기가 완료되었습니다");
+            // 로그인 페이지로 이동
+            window.location.href = "../index/index.html";
+        } else {
+            // 입력이 없을 경우 알림창 표시
+            alert("아이디와 이름을 입력하세요");
+        }
+    });
+});
